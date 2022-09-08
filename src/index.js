877ce5af38d1607896b09b03535426c6b7b92b3c
Sumanto Banerjee <sbanerjee@Sumantos-MacBook-Pro.local>
2022-09-08 19:19:42 +0530

    Subscribe to the store

--- src/index.js
+++ src/index.js
@@ -9,19 +9,11 @@ import movies from './reducers'
 
 const store = createStore(movies);
 console.log('store', store)
-console.log('BEFORE STATE', store.getState())
-
-store.dispatch({
-  type: 'ADD_MOVIES',
-  movies: [{ name: 'superman' }]
-})
-
-console.log('AFTER STATE', store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
 
