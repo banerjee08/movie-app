3b655d61757bb30090b42e17446a6ba677cddf9f
Sumanto Banerjee <sbanerjee@Sumantos-MacBook-Pro.local>
2022-09-14 11:31:10 +0530

    favouriting movie

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,37 +1,49 @@
 import React from 'react';
-
+import { addFavourite } from '../actions';
 
 class MovieCard extends React.Component {
+  handleFavouriteCLick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(addFavourite(movie));
+  };
 
-    render(){
-        const { movie } = this.props;
-        return (
-            <div className="movie-card">
-              {/* console.log({movie.Plot}) */}
-              <div className="left">
-                <img alt="movie-poster" src={movie.Poster} />
-              </div>
+  handleUnFavouriteCLick = () => {}
+  render() {
+    const { movie, isFavourite } = this.props;
+    return (
+      <div className="movie-card">
+        {/* console.log({movie.Plot}) */}
+        <div className="left">
+          <img alt="movie-poster" src={movie.Poster} />
+        </div>
 
-              <div className="right">
-                <div className="title">
-                    {movie.Title}
-                </div>
-                
-                <div className="plot">
-                    {movie.Plot}
-                </div>
-                
-                <div className="footer">
-                    <div className="rating">
-                        {movie.imdbRating}
-                    </div>
-                    <button className="favourite-btn">Favourite</button>
-                </div>
-              </div>
-            </div>
-          );
-    }
-  
+        <div className="right">
+          <div className="title">{movie.Title}</div>
+
+          <div className="plot">{movie.Plot}</div>
+
+          <div className="footer">
+            <div className="rating">{movie.imdbRating}</div>
+            {isFavourite ? (
+              <button
+                className="unfavourite-btn"
+                onClick={this.handleUnFavouriteCLick}
+              >
+                Unfavourite
+              </button>
+            ) : (
+              <button
+                className="favourite-btn"
+                onClick={this.handleFavouriteCLick}
+              >
+                Favourite
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MovieCard;
