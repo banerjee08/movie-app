aecd0f4ffe9d8afa924952b66bd601217c2bd438
Sumanto Banerjee <sbanerjee@Sumantos-MacBook-Pro.local>
2022-09-17 17:54:51 +0530

    Using context API-I

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 // import { createStore } from 'redux';
-import { legacy_createStore as createStore, applyMiddleware} from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
@@ -23,30 +23,44 @@ import rootReducer from './reducers';
 // }
 
 // another way of writing the middleware logger function
-const logger = ({dispatch, getState}) => (next) => (action) => {
-  if(typeof action !== 'function')
-{
-  console.log('ACTION_TYPE: ', action.type);
-}  
-  next(action)
-}
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  if (typeof action !== 'function') {
+    console.log('ACTION_TYPE: ', action.type);
+  }
+  next(action);
+};
 
-const thunk =  ({dispatch, getState}) => (next) => (action) => {
-  if(typeof action === 'function'){
+const thunk = ({ dispatch, getState }) => (next) => (action) => {
+  if (typeof action === 'function') {
     action(dispatch);
     return;
   }
   next(action);
-}
-
+};
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
-console.log('store', store)
+console.log('store', store);
+
+export const StoreContext = createContext();
+console.log('StoreContext', StoreContext);
+
+class Provider extends React.Component {
+  render() {
+    const { store } = this.props;
+    return <StoreContext.Provider value={store}>
+      {this.props.childeren}
+    </StoreContext.Provider>;
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App store={store} />
+    {/* <StoreContext.Provider value={store}> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+    {/* </StoreContext.Provider> */}
   </React.StrictMode>
 );
 
